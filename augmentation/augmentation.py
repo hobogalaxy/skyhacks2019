4aa88ba43003a8aeccb30f1810af1e861c49d900
Micho6 <michal.dudkiewicz96@gmail.com>
2019-10-26 14:55:10 +0200

    csv augmentation

--- augmentation/augmentation.py
+++ augmentation/augmentation.py
@@ -2,12 +2,14 @@ from keras_preprocessing.image import load_img
 import glob
 import numpy as np
 from keras_preprocessing.image import ImageDataGenerator
-import time
+import pandas as pd
+import os
+from matplotlib import pyplot as plt
 
 
 def load_images(path):
     image_list = []
-    for filename in glob.glob(path + "/*.jpg"):
+    for filename in sorted(glob.glob(path + "/*.jpg")):
         img = load_img(filename)
 
         img = img.resize((300, 300))
@@ -15,10 +17,11 @@ def load_images(path):
         img = np.asarray(img)
 
         image_list.append(img)
-
     images = np.array(image_list)
     print(images.shape)
     # images = np.expand_dims(images, axis=0)
+    plt.imshow(images[0])
+    plt.show()
     return images
 
 
@@ -43,9 +46,20 @@ def augmentate(path, save_dir, new_img_num):
         if total == new_img_num:
             break
 
+#
+# augmentate("../dataset/bedroom/",  "../test/", 5)
+#
+# # print(str(end - now) + "s")
+my_directory = "../test/"
+data = pd.read_csv('./labels.csv')
+path = "../dataset/bedroom/"
+licznik = 0
+dictionary = {}
+for filename in sorted(glob.glob(path + "/*.jpg")):
+    for aug_filename in sorted(glob.glob(my_directory + "/*.jpg"))[5 * licznik: 5 * licznik + 5]:
+            dictionary[aug_filename[8:]] = data.loc[data['filename'] == filename[-44:]]
+            # print(pd.DataFrame(data.loc[data['filename'] == filename[-44:]]))
+    licznik += 1
 
-# now = time.time()
-augmentate("/home/adrian/skyhacks/data/main_task_data/bathroom/",  "/home/adrian/skyhacks/test/", 5)
-# end = time.time()
 
-# print(str(end - now) + "s")
+print(dictionary)
